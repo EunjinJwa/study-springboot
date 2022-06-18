58edf83c0aaf4145567ed701b4ef3951b0ccd10f
EunjinJwa <ejjwa0563@gmail.com>
2022-06-18 23:06:14 +0900

    google oauth 로그인 후 처리 : loadUser()

--- basic/src/main/java/springboot/security/basic/config/SecurityConfig.java
+++ basic/src/main/java/springboot/security/basic/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package springboot.security.basic.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -7,12 +8,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import springboot.security.basic.config.oauth.PrincipalOauth2UserService;
 
 @Configuration
 @EnableWebSecurity  // Spring Security filter가 Spring filter chain에 등록됨
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)  // secured 어노테이션 활성화, prePostEnabled => preAuthorize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -26,7 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/")
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm");   // google 로그인 완료 후 후처리가 필요함
+                .loginPage("/loginForm")
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService); //  구글 로그인이 완료된 뒤의 후처리가필요함. Tip. (엑세스 토큰 + 사용자프로필정)
     }
 
     @Bean
