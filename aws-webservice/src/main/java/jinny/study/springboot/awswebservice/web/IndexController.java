98e459aa2086fd29316830a6515fd0947d89da48
EunjinJwa <ejjwa0563@gmail.com>
2022-08-09 18:50:43 +0900

    @LoginUser 어노테이션 사용 및 parameterResolver 생성

--- aws-webservice/src/main/java/jinny/study/springboot/awswebservice/web/IndexController.java
+++ aws-webservice/src/main/java/jinny/study/springboot/awswebservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package jinny.study.springboot.awswebservice.web;
 
+import jinny.study.springboot.awswebservice.config.auth.LoginUser;
 import jinny.study.springboot.awswebservice.config.auth.SessionUser;
 import jinny.study.springboot.awswebservice.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
